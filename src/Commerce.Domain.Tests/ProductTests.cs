1544ce2b3c102b40f57643f136dcc9d89325d960
べくゾド <begzodpublic@gmail.com>
2026-09-03 15:22:31 +0900

    added entities and tests

---
+++ src/Commerce.Domain.Tests/ProductTests.cs
@@ -0,0 +1,70 @@
+﻿using Commerce.Domain.Entities;
+
+namespace Commerce.Domain.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CreateProduct_WithValidData_ShouldCreateProduct()
+        {
+            // Arrange
+            var product = new Product(
+                "iPhone 17",
+                "Apple smartphone",
+                999.99m,
+                10);
+
+            // Act
+            var result = product;
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, result.Id);
+            Assert.Equal("iPhone 17", result.Name);
+            Assert.Equal(999.99m, result.Price);
+            Assert.Equal(10, result.Stock);
+            Assert.True(result.IsActive);
+        }
+
+        [Fact]
+        public void CreateProduct_WithInvalidPrice_ShouldThrowException()
+        {
+            // Arrange
+            var action = () => new Product(
+                "iPhone 17",
+                "Apple smartphone",
+                -100,
+                10);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void CreateProduct_WithNegativeStock_ShouldThrowException()
+        {
+            // Arrange
+            var action = () => new Product(
+                "iPhone 17",
+                "Apple smartphone",
+                999.99m,
+                -1);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyName_ShouldThrowException()
+        {
+            // Arrange
+            var action = () => new Product(
+                "",
+                "Apple smartphone",
+                999.99m,
+                10);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(action);
+        }
+    }
+}
---
+++ src/Commerce.Domain/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Commerce.Domain.Entities
+{
+    public class Product
+    {
+        public Product(string name, string description, decimal price, int stock)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Product name cannot be empty");
+            if (price <= 0)
+                throw new ArgumentException("Price must be greater than zero.");
+
+            if (stock < 0)
+                throw new ArgumentException("Stock cannot be negative.");
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            IsActive = true;
+            CreatedAt = DateTime.UtcNow;
+        }
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price { get; private set; }
+
+        public int Stock { get; private set; }
+
+        public bool IsActive { get; private set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
