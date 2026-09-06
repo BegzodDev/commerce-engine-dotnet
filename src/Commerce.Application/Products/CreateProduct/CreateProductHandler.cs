caafa65c398fdff6c94d039ba08c12f4cfe66eeb
べくゾド <begzodpublic@gmail.com>
2026-09-07 01:31:34 +0900

    .

---
+++ src/Commerce.Application/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,13 @@
+﻿
+using Commerce.Domain.Entities;
+
+namespace Commerce.Application.Products.CreateProduct
+{
+    public class CreateProductHandler
+    {
+        public Product Handle(CreateProductCommand command)
+        {
+
+        }
+    }
+}
