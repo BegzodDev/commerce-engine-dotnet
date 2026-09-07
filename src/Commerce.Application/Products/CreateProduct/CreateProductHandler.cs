bd2dc036eb6864983b63274e656ada0402af3995
べくゾド <begzodpublic@gmail.com>
2026-09-08 02:05:25 +0900

    Added database and migrations

--- src/Commerce.Application/Products/CreateProduct/CreateProductHandler.cs
+++ src/Commerce.Application/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,33 @@
 ﻿
+using Commerce.Application.Products.Interfaces;
 using Commerce.Domain.Entities;
 
 namespace Commerce.Application.Products.CreateProduct
 {
     public class CreateProductHandler
     {
-        public Product Handle(CreateProductCommand command)
-        {
+        /// <summary>
+        /// Handler ishlashi uchun menga IProductRepository kerak (yani pasidagi qismi)
+        /// </summary>
+        private readonly IProductRepository _repository;
 
+
+        public CreateProductHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task<Product> Handle(CreateProductCommand command)
+        {
+            var product = new Product(
+            command.Name,
+            command.Description,
+            command.Price,
+            command.Stock);
+
+            await _repository.AddAsync(product);
+            return product;
         }
     }
 }
