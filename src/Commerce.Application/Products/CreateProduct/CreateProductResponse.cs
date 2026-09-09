9d4ab23b069a369b3bbbf99b112829b812cd3f97
べくゾド <begzodpublic@gmail.com>
2026-09-09 23:24:59 +0900

    Added request and response models

---
+++ src/Commerce.Application/Products/CreateProduct/CreateProductResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Commerce.Application.Products.CreateProduct
+{
+    public class CreateProductResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
